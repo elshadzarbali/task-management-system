ced5cfe73b66103318bc255ae91a740efad353e7
Elshad Zarbalizade <elshadzarbali@gmail.com>
2024-05-10 11:54:04 +0400

    Added findByUsername and findByEmail

--- src/main/java/com/example/repository/UserRepository.java
+++ src/main/java/com/example/repository/UserRepository.java
@@ -4,4 +4,6 @@ import com.example.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
+    User findByUsername(String username);
+    User findByEmail(String email);
 }
